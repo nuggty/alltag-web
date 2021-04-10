55b8898055a262a04ea8436c8bd0ab9730cfd2a1
Robert Pereira <53867960+nuggty@users.noreply.github.com>
2021-04-10 00:22:53 -0300

    Só... arrumando as dependências

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,6 @@ import App from 'next/app'
 import { Provider as StyletronProvider } from 'styletron-react'
 import { styletron } from '../styletron'
 
-if (typeof window !== "undefined") {
-  require("popper.js");
-  require("baseui");
-  require('styletron-engine-atomic');
-}
-
 export default class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props
